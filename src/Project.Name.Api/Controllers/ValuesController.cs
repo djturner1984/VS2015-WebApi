e470393e8f0f1af89f14b6835efac837bdb801d0
David Turner <david.turner@fairfaxmedia.com.au>
2015-11-29 15:30:53 +1100

    initial commit

---
+++ src/Project.Name.Api/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Cors;
+using Microsoft.AspNet.Mvc;
+
+namespace Project.Name.Controllers
+{
+    [Route("[controller]")]
+    [EnableCors("Allow")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        [Route("Get")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
